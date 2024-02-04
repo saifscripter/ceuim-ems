b042991991b8e4d0b6d364afd90fa2efe493d525
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-02-04 06:35:32 +0600

    update sidebar functionality

--- src/components/Header/Navlink.jsx
+++ src/components/Header/Navlink.jsx
@@ -2,13 +2,14 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function Navlink({ href, text }) {
+export default function Navlink({ href, text, ...restProps }) {
   const pathname = usePathname();
 
   return (
     <Link
       href={href}
       className={`hover:text-primary ${pathname === href && "text-primary"}`}
+      {...restProps}
     >
       {text}
     </Link>
--- src/components/Header/Sidebar.jsx
+++ src/components/Header/Sidebar.jsx
@@ -1,16 +1,28 @@
 "use client";
 
 import { leftMenu, rightMenu } from "@/data/menu";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { VscChromeClose, VscThreeBars } from "react-icons/vsc";
 import Navlink from "./Navlink";
 
 export default function Sidebar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const sidebarRef = useRef(null);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
+
+  useEffect(() => {
+    const hideSidebar = () => setIsSidebarOpen(false);
+
+    document.body.addEventListener("click", hideSidebar);
+    sidebarRef.current.addEventListener("click", (e) => e.stopPropagation());
+
+    return () => {
+      document.body.removeEventListener("click", hideSidebar);
+    };
+  }, []);
 
   return (
     <div>
@@ -27,13 +39,24 @@ export default function Sidebar() {
         className={`bg-black text-white flex flex-col gap-6 absolute top-0 left-0 h-screen w-72 p-5  transition-transform duration-100 ease-out ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
         }`}
+        ref={sidebarRef}
       >
         {leftMenu.map(({ text, href }) => (
-          <Navlink href={href} text={text} key={href} />
+          <Navlink
+            href={href}
+            text={text}
+            key={href}
+            onClick={() => setIsSidebarOpen(false)}
+          />
         ))}
 
         {rightMenu.map(({ text, href }) => (
-          <Navlink href={href} text={text} key={href} />
+          <Navlink
+            href={href}
+            text={text}
+            key={href}
+            onClick={() => setIsSidebarOpen(false)}
+          />
         ))}
       </div>
     </div>
