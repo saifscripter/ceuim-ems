6dadfb9b3f3d0e48681b76c55d9315887ff017f8
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-02-04 07:25:10 +0600

    fix reload issue on sidebar navigation

--- src/components/Header/Sidebar.jsx
+++ src/components/Header/Sidebar.jsx
@@ -1,23 +1,25 @@
 "use client";
 
 import { leftMenu, rightMenu } from "@/data/menu";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { VscChromeClose, VscThreeBars } from "react-icons/vsc";
 import Navlink from "./Navlink";
 
 export default function Sidebar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const sidebarRef = useRef(null);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   useEffect(() => {
-    const hideSidebar = () => setIsSidebarOpen(false);
+    const hideSidebar = (event) => {
+      if (event.target.id !== "sidebar") {
+        setIsSidebarOpen(false);
+      }
+    };
 
     document.body.addEventListener("click", hideSidebar);
-    sidebarRef.current.addEventListener("click", (e) => e.stopPropagation());
 
     return () => {
       document.body.removeEventListener("click", hideSidebar);
@@ -36,10 +38,10 @@ export default function Sidebar() {
       )}
 
       <div
+        id="sidebar"
         className={`bg-black text-white flex flex-col gap-6 absolute top-0 left-0 h-screen w-72 p-5  transition-transform duration-100 ease-out ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
         }`}
-        ref={sidebarRef}
       >
         {leftMenu.map(({ text, href }) => (
           <Navlink
